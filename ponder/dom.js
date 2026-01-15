05d4c39156bc0f8ffcdd892801dc6a310a4d2a32
elysianlin <lin24024@byui.edu>
2026-01-14 19:51:58 -0700

    update the js with dom ponder

--- ponder/dom.js
+++ ponder/dom.js
@@ -22,10 +22,36 @@ let selectElem = document.getElementById('webdevlist');
 selectElem.addEventListener('change', function(){
     let codeValue = selectElem.value;
     console.log(codeValue);
-    document.querySelector('#html').style.color = 'red';
+    document.querySelector('#html').style.color = '';
+    document.querySelector('#css').style.color = '';
+    document.querySelector('#js').style.color = '';
+
+    document.getElementById(codeValue).style.color = 'red';
 })
 
 const newpara = document.createElement('p');
 newpara.innerText = 'Added with JavaScript';
 document.body.appendChild(newpara);
+
+const newImage = document.createElement('img');
+newImage.src = ('src', 'https://picsum.photos/200');
+document.body.appendChild(newImage);
+
+const newDiv = document.createElement('div');
+newDiv.innerHTML = '<ul><li>One</li><li>Two</li><li>Three</li></ul>';
+document.body.appendChild(newDiv);
+
+const section = document.querySelector('section');
+section.innerHTML = '<h2>DOM Basics <h2><p>This was also added through JS</p>';
+document.body.appendChild(section);
+
+const btn = document.getElementById('btn');
+const text = document.getElementById('texxt');
+
+btn.addEventListener('click', () => {
+    text.classList.toogle('hightlight');
+});
+
+const username = document.querySelector('#name');
+username.value = 'me';
                 
