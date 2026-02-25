ba508e48b69d3f3f8459a00f7af13c3e58b7d642
elysianlin <lin24024@byui.edu>
2026-02-25 16:17:57 -0700

    character card

---
+++ charactercard/script.js
@@ -0,0 +1,43 @@
+const character = {
+    name: "Snortleblat",
+    class: "Swamp Beast Ciplomat",
+    level:5,
+    Health: 100,
+    image: "/images/snortleblat.webp",
+    attack: function() {
+        if(this.Health >0) {
+            this.Health -= 20;
+            if (this.Health <= 0) {
+                this.Health =0;
+                alert(`${this.name} has died!`);
+            }
+            this.displayCharacter();
+        }
+    },
+
+    levelUp: function() {
+        this.level += 1;
+        this.displayCharacter();
+    },
+
+    displayCharacter: function() {
+        const cardElement = document.querySelector('#CharacterCard');
+        cardElement.innerHTML = `
+        <div class="card">
+            <img src="${this.image}" alt="${this.name}" class="image">
+            <div class="name" <h2>${this.name}</h2></div>
+            <div class="stats">
+                <p><strong>Class: ${this.class}</strong></p>
+                <p><strong>Level: ${this.level}</strong></p>
+                <p><strong>Health: ${this.Health}</strong></p>
+            </div>
+            <div class="buttons">
+                <button onclick="character.attack()" id="attackBtn">Attack</button>
+                <button onclick="character.levelUp()" id="levelUpBtn">Level Up</button>
+            </div> 
+        </div>
+        `;
+    }
+};
+
+character.displayCharacter();
